31a5e247990071b3accce994c8f60d1438e528e5
codewithxavi <javieradrian.it@gmail.com>
2023-06-12 16:26:05 +0200

    Fix bad things

--- src/components/BannerSpash.jsx
+++ src/components/BannerSpash.jsx
@@ -7,12 +7,12 @@ const BannerSplash = () => {
       <div className="flex flex-col md:flex-row">
         <div className="w-full  flex items-center justify-center">
           <NavLink to="/series">
-            <img src="/splash/poster-1.png" alt="Image 1" className="h-full w-full object-contain" />
+            <img src="/splash/poster-1.webp" alt="Image 1" className="h-full w-full object-contain" />
           </NavLink>
         </div>
         <div className="w-full  md:order-last flex items-center justify-center">
           <NavLink to="/movies">
-            <img src="/splash/poster.png" alt="Image 1" className="h-full w-full object-contain" />
+            <img src="/splash/poster.webp" alt="Image 1" className="h-full w-full object-contain" />
           </NavLink>
         </div>
       </div>
